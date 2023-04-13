99915110bfcd607f57cfb45df7ced914abfa8377
Josué <josue.dev@outlook.fr>
2023-04-13 19:33:58 +0200

    Refactorisation du code

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 200,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text(
-                'Apprendre Flutter c\'est cool ! ',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Appuyez-ici',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
+          child: const StartScreen(),
         ),
       ),
     ),
---
+++ lib/start_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'Apprendre Flutter c\'est cool !',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              child: const Text(
+                'Appuyez-ici',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}

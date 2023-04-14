af2d0b94390c29afe564b43eca098e225e0b9f98
Josué <josue.dev@outlook.fr>
2023-04-14 20:37:37 +0200

    Changement des themes de couleurs

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,10 @@ void main() {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
+                Color.fromARGB(255, 187, 3, 204),
+                Color.fromARGB(255, 130, 3, 204),
+                Color.fromARGB(255, 90, 3, 204),
+                Color.fromARGB(255, 15, 94, 168),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
